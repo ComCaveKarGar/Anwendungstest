06bce5cdbbf4de375ece88a0f054323b9392d5e6
Karsten <Moonjumper@gmx.de>
2023-05-09 19:31:29 +0200

    Name und Alter ergänzt via prompt

--- script01.js
+++ script01.js
@@ -1,1 +1,10 @@
+"use strict";
+
+let firstName, familyName, age;
+
+firstName = prompt("wie ist dein Vorname?");
+familyName = prompt("Wie ist dein Nachname?");
+age = prompt("Wie alt bist du?")
+
+console.log("Ich bin" + " " + firstName + " " + familyName + ", und bin " + age + " " + "alt.");
 
