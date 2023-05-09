2d88cd3026540289e742fbcbaa4a239279008228
Karsten <Moonjumper@gmx.de>
2023-05-09 19:37:53 +0200

    Name und Alter + Text

--- script01.js
+++ script01.js
@@ -1,10 +1,13 @@
 "use strict";
 
+console.log("Hallo zusammen!");
+
 let firstName, familyName, age;
 
 firstName = prompt("wie ist dein Vorname?");
 familyName = prompt("Wie ist dein Nachname?");
-age = prompt("Wie alt bist du?")
+age = prompt("Wie alt bist du?");
 
 console.log("Ich bin" + " " + firstName + " " + familyName + ", und bin " + age + " " + "alt.");
 
+console.log("Wie geht es dir?");
